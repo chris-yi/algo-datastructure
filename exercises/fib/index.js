10d9b5aab0d85e6965953f6aa0fa3a5663f9f3dc
chris-yi <kri5to.yi@gmail.com>
2018-05-17 17:51:55 -0600

    first solution to fibonnaci

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,31 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// First solution
+// function fib(n) {
+//     const fibArr = [0, 1];
+//     for(let i = 2; i <= n ; i++) {
+//       let num = 1
+//       num = fibArr[i - 2] + fibArr[i - 1]
+//       fibArr.push(num)
+//     }
+//     return fibArr[n]
+// }
+
+// Better iterative solution
+// Runtime complexity === linear runtime (n)
+function fib(n) {
+    const result = [0, 1];
+
+    for(let i = 2; i <= n; i++) {
+        const a = result[i - 1]
+        const b = result[i - 2]
+
+        result.push(a + b)
+    }
+    return result[n]
+}
+
+
 
 module.exports = fib;
