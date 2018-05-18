6e0663b7d5d98b560df4e806399af7917969d47c
chris-yi <kri5to.yi@gmail.com>
2018-05-17 18:05:26 -0600

    fibonnaci recursive solution

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -21,16 +21,25 @@
 
 // Better iterative solution
 // Runtime complexity === linear runtime (n)
+// function fib(n) {
+//     const result = [0, 1];
+
+//     for(let i = 2; i <= n; i++) {
+//         const a = result[i - 1]
+//         const b = result[i - 2]
+
+//         result.push(a + b)
+//     }
+//     return result[n]
+// }
+
+
+// Solution using Recursion
 function fib(n) {
-    const result = [0, 1];
-
-    for(let i = 2; i <= n; i++) {
-        const a = result[i - 1]
-        const b = result[i - 2]
-
-        result.push(a + b)
+    if(n < 2) {
+        return n
     }
-    return result[n]
+    return fib(n -1) + fib(n - 2)
 }
 
 
