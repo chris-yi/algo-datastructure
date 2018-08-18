ce4665deb50a8a67a5c47ee498bdc185055b5ce0
chris-yi <kri5to.yi@gmail.com>
2018-08-18 13:28:52 -0600

    redo fizzBuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,16 +12,16 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {
+const fizzBuzz = (n) => {
   for (let i = 1; i <= n; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log(`fizzbuzz`);
+    if (i % 15 === 0) {
+      console.log("fizzbuzz")
     } else if (i % 5 === 0) {
-      console.log(`buzz`);
+      console.log("buzz")
     } else if (i % 3 === 0) {
-      console.log(`fizz`);
-    } else {
-      console.log(i);
+      console.log("fizz")
+    } else  {
+      console.log(i)
     }
   }
 }
