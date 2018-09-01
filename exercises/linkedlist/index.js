6c6f84c2a3d60b0beec81eb73da15e6a7f04a477
chris-yi <kri5to.yi@gmail.com>
2018-09-01 17:44:35 -0600

    linkedList restart

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -3,96 +3,11 @@
 // See 'directions' document
 
 class Node {
-  constructor(data, next = null) {
-    this.data = data;
-    this.next = next;
-  }
+
 }
 
 class LinkedList {
-  constructor() {
-    this.head = null;
-  }
 
-  insertFirst(data) {
-    // You can write as a one liner like below:
-    // this.head = new Node(data, this.head);
-
-    const node = new Node(data, this.head);
-    this.head = node;
-  }
-
-  size() {
-    let counter = 0;
-    let node = this.head;
-
-    while (node) {
-      counter++;
-      node = node.next;
-    }
-    return counter;
-  }
-
-  getFirst() {
-    return this.head;
-  }
-
-  getLast() {
-    if (!this.head) {
-      return null;
-    }
-
-    let node = this.head;
-
-    while (node) {
-      if (!node.next) {
-        return node;
-      }
-      node = node.next;
-    }
-  }
-
-  clear() {
-    this.head = null;
-  }
-
-  removeFirst() {
-    if (!this.head) {
-      return;
-    }
-
-    this.head = this.head.next;
-  }
-
-  removeLast() {
-    if (!this.head) {
-      return;
-    }
-
-    if (!this.head.next) {
-      this.head = null;
-      return;
-    }
-
-    let previous = this.head;
-    let node = this.head.next;
-
-    while (node.next) {
-      previous = node;
-      node = node.next;
-    }
-
-    previous.next = null;
-  }
-
-  insertLast(data) {
-    const last = this.getLast();
-    if (last) {
-      last.next = new Node(data);
-    } else {
-      this.head = new Node(data);
-    }
-  }
   
 }
 
